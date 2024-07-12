108c5076d1f3cc6642b1c9ea597e513ca52438df
hseong3243 <hseong3243@gmail.com>
2024-07-12 23:08:37 +0900

    깃 저장소 초기화
    프로젝트명: 24시간 안에 뚝딱 만드는 현장 판매 및 재고 관리 시스템

---
+++ src/main/java/com/hseong/jungleproduct/JungleProductApplication.java
@@ -0,0 +1,13 @@
+package com.hseong.jungleproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungleProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JungleProductApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hseong/jungleproduct/JungleProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hseong.jungleproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JungleProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
