70f9ebf5e83420a29fe1ec2a323abf8f150cdbc6
hseong3243 <hseong3243@gmail.com>
2024-07-14 23:47:43 +0900

    feat: 상품 목록 조회 api 호출 구현

--- frontend/src/component/product/ProductPage.vue
+++ frontend/src/component/product/ProductPage.vue
@@ -2,6 +2,7 @@
 import OrderCard from "@/component/order/OrderCard.vue";
 import ProductCard from "@/component/product/ProductCard.vue";
 import AddProductDialog from "@/component/product/AddProductDialog.vue";
+import api from "@/axios/index.js";
 
 export default {
   name: "ProductPage",
@@ -16,8 +17,19 @@ export default {
           displayAmount: 0,
           storageAmount: 0
         }
-      ]
+      ],
+      ready: false
     }
+  },
+  methods: {
+    async findAllProducts() {
+      let response = await api.get('api/products');
+      this.products = response.data.data;
+    }
+  },
+  async mounted() {
+    await this.findAllProducts()
+    this.ready = true;
   }
 }
 </script>
@@ -29,7 +41,7 @@ export default {
         <v-text-field variant="outlined" density="compact" label="품번"></v-text-field>
       </div>
     </div>
-    <div class="d-flex flex-wrap">
+    <div class="d-flex flex-wrap" v-if="ready">
       <ProductCard
           v-for="product in products"
           :key="product.productId"
