8f01536c43905554a4ae4bfd4be0078613426d79
hseong3243 <hseong3243@gmail.com>
2024-07-14 17:31:20 +0900

    feat: 재고 관리 페이지 구현

---
+++ frontend/src/component/ProductCard.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: "ProductCard",
+  props: ['product']
+}
+</script>
+
+<template>
+  <div class="d-flex flex-column card">
+    <div class="d-flex justify-space-between mb-1">
+      <div>no. {{ product.productId }}</div>
+      <div>{{ product.price }}원</div>
+    </div>
+    <div>상품명: {{ product.name }}</div>
+    <div>전시 수량: {{ product.displayAmount }}</div>
+    <div>박스 재고: {{ product.storageAmount }}</div>
+    <div class="d-flex justify-end ga-1 mt-1">
+      <v-btn variant="tonal" color="blue-lighten-1">재고 관리</v-btn>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  border: #d9d9d9 2px solid;
+  border-radius: 10px;
+  padding: 10px;
+  width: 45%;
+}
+</style>
---
+++ frontend/src/component/ProductPage.vue
@@ -0,0 +1,58 @@
+<script>
+import OrderCard from "@/component/OrderCard.vue";
+import ProductCard from "@/component/ProductCard.vue";
+
+export default {
+  name: "ProductPage",
+  components: {ProductCard, OrderCard},
+  data() {
+    return {
+      products: [
+        {
+          productId: 2324,
+          name: '미니얼룩말',
+          price: 24000,
+          displayAmount: 0,
+          storageAmount: 0
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+<div>
+  <div>
+    <v-container>
+      <v-row no-gutters class="align-center">
+        <v-col cols="3">
+          <div class="link">구매 관리</div>
+        </v-col>
+        <v-col cols="6" class="text-center">
+          <div class="text-h6">정글 재고 관리</div>
+        </v-col>
+        <v-col cols="3">
+          <v-btn variant="tonal" color="green-lighten-1">상품 등록</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+    <div class="d-flex justify-center">
+      <div class="w-75">
+        <v-text-field variant="outlined" density="compact" label="품번"></v-text-field>
+      </div>
+    </div>
+    <div class="d-flex flex-wrap">
+      <ProductCard
+          v-for="product in products"
+          :key="product.productId"
+      :product="product">
+      </ProductCard>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
