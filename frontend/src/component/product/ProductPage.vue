174888adc594af2ec48a3538525ea50964ab733f
hseong3243 <hseong3243@gmail.com>
2024-07-14 23:59:59 +0900

    feat: 품번 프리픽스 검색 api 구현

--- frontend/src/component/product/ProductPage.vue
+++ frontend/src/component/product/ProductPage.vue
@@ -3,12 +3,14 @@ import OrderCard from "@/component/order/OrderCard.vue";
 import ProductCard from "@/component/product/ProductCard.vue";
 import AddProductDialog from "@/component/product/AddProductDialog.vue";
 import api from "@/axios/index.js";
+import {debounce} from "lodash";
 
 export default {
   name: "ProductPage",
   components: {AddProductDialog, ProductCard, OrderCard},
   data() {
     return {
+      keyword: "",
       products: [
         {
           productId: 2324,
@@ -21,10 +23,28 @@ export default {
       ready: false
     }
   },
+  watch: {
+    keyword: debounce(function (val, oldVal) {
+      this.searchProducts(val)
+    }, 200)
+  },
   methods: {
     async findAllProducts() {
-      let response = await api.get('api/products');
+      let response = await api.get('/api/products');
       this.products = response.data.data;
+    },
+    async searchProducts(prefix) {
+      if(prefix === "") {
+        await this.findAllProducts()
+        return;
+      }
+      let response = await api.get('/api/products/search', {
+        params: {
+          productId: prefix
+        }
+      });
+      this.products = response.data.data;
+      console.log(this.products)
     }
   },
   async mounted() {
@@ -38,7 +58,11 @@ export default {
   <div>
     <div class="d-flex justify-center">
       <div class="w-75">
-        <v-text-field variant="outlined" density="compact" label="품번"></v-text-field>
+        <v-text-field
+            v-model="keyword"
+            variant="outlined"
+            density="compact"
+            label="품번"></v-text-field>
       </div>
     </div>
     <div class="d-flex flex-wrap" v-if="ready">
