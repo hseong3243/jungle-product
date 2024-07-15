5a9d4947156061afb2a6c2d219c18d127188facc
hseong3243 <hseong3243@gmail.com>
2024-07-15 23:23:15 +0900

    feat: 상품 jpa 엔티티 구현
    상품 번호 프리픽스 검색을 위해 인덱스를 생성함.

---
+++ src/main/java/com/hseong/jungleproduct/repository/ProductEntity.java
@@ -0,0 +1,48 @@
+package com.hseong.jungleproduct.repository;
+
+import com.hseong.jungleproduct.domain.Product;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "product", indexes = @Index(name = "idx_product_number", columnList = "productNumber"))
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    private String productNumber;
+    private String name;
+    private int price;
+    private int displayAmount = 0;
+    private int storageAmount = 0;
+
+    private ProductEntity(String productNumber, String name, int price, int displayAmount, int storageAmount) {
+        this.productNumber = productNumber;
+        this.name = name;
+        this.price = price;
+        this.displayAmount = displayAmount;
+        this.storageAmount = storageAmount;
+    }
+
+    public static ProductEntity from(Product product) {
+        return new ProductEntity(product.getProductId().toString(), product.getName(), product.getPrice(),
+                product.getDisplayAmount(), product.getStorageAmount());
+    }
+
+    public Product toDomain() {
+        Product product = new Product(Long.parseLong(productNumber), name, price);
+        product.initializeInventory(displayAmount, storageAmount);
+        return product;
+    }
+}
