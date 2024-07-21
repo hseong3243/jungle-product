8740e1619d55bb30657ddddcdceab4f7357e1940
hseong3243 <hseong3243@gmail.com>
2024-07-21 19:38:48 +0900

    feat: 로그인 컨트롤러 구현

--- src/main/java/com/hseong/jungleproduct/controller/request/LoginRequest.java
+++ src/main/java/com/hseong/jungleproduct/controller/request/LoginRequest.java
@@ -1,2 +1,5 @@
-package com.hseong.jungleproduct.controller.request;public record LoginRequest() {
+package com.hseong.jungleproduct.controller.request;
+
+public record LoginRequest(String username, String password) {
+
 }
