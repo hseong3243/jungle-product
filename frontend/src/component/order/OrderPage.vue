1e4feca5005d0976e6e6a16ec19f6fb215ebc060
hseong3243 <hseong3243@gmail.com>
2024-07-21 20:08:31 +0900

    feat: 상품, 주문 페이지 분리
    기존 App.vue에서 분기처리하던 페이지를 별도의 페이지로 분리함.

---
+++ frontend/src/component/order/OrderPage.vue
@@ -0,0 +1,31 @@
+<script>
+import OrderSector from "@/App.vue";
+
+export default {
+  name: "OrderPage",
+  components: {OrderSector}
+}
+</script>
+
+<template>
+<v-container>
+  <v-row>
+    <v-col cols="3">
+      <div class="link">주문 관리</div>
+    </v-col>
+  </v-row>
+  <v-col cols="6">
+    <div class="text-h6 text-center">정글 주문 관리</div>
+  </v-col>
+  <v-col cols="3">
+
+  </v-col>
+  <v-row>
+    <OrderSector/>
+  </v-row>
+</v-container>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/component/product/ProductPage.vue
@@ -0,0 +1,32 @@
+<script>
+import AddProductDialog from "@/component/product/AddProductDialog.vue";
+import ProductSector from "@/App.vue";
+
+export default {
+  name: "ProductPage",
+  components: {ProductSector, AddProductDialog}
+}
+</script>
+
+<template>
+<v-container>
+  <v-row>
+    <v-col cols="3">
+      <div class="link">상품 관리</div>
+    </v-col>
+    <v-col cols="6">
+      <div class="text-h6 text-center">정글 재고 관리</div>
+    </v-col>
+    <v-col cols="3">
+      <AddProductDialog v-if="page === 'PRODUCT'"/>
+    </v-col>
+  </v-row>
+  <v-row>
+    <ProductSector/>
+  </v-row>
+</v-container>
+</template>
+
+<style scoped>
+
+</style>
