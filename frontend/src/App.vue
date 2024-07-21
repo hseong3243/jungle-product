eac4e7d66e1d44b58777a30aff08de9ee3f827c4
hseong3243 <hseong3243@gmail.com>
2024-07-21 23:55:35 +0900

    feat: App.vue 뷰 라우터 적용

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -26,23 +26,7 @@ export default {
 
 <template>
   <div id="app" class="pa-2 d-flex flex-column">
-    <v-container>
-      <v-row no-gutters class="align-center">
-        <v-col cols="3">
-          <div class="link" @click="selectOrderSector" v-if="page === 'PRODUCT'">주문 관리</div>
-          <div class="link" @click="selectProductSector" v-if="page === 'ORDER'">상품 관리</div>
-        </v-col>
-        <v-col cols="6">
-          <div class="text-h6 text-center" v-if="page === 'ORDER'">정글 주문 관리</div>
-          <div class="text-h6 text-center" v-if="page === 'PRODUCT'">정글 재고 관리</div>
-        </v-col>
-        <v-col cols="3">
-          <AddProductDialog v-if="page === 'PRODUCT'"/>
-        </v-col>
-      </v-row>
-    </v-container>
-    <ProductSector v-if="page === 'PRODUCT'"/>
-    <OrderSector v-if="page === 'ORDER'"/>
+    <router-view/>
   </div>
 </template>
 
