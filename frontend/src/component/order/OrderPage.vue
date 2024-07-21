12cb8ee92560e36f49ceee983ad1257a92423190
hseong3243 <hseong3243@gmail.com>
2024-07-21 23:53:34 +0900

    fix: OrderPage, ProductPage 잘못된 임포트 수정

--- frontend/src/component/order/OrderPage.vue
+++ frontend/src/component/order/OrderPage.vue
@@ -1,5 +1,5 @@
 <script>
-import OrderSector from "@/App.vue";
+import OrderSector from "@/component/order/OrderSector.vue";
 
 export default {
   name: "OrderPage",
@@ -8,22 +8,24 @@ export default {
 </script>
 
 <template>
-<v-container>
-  <v-row>
-    <v-col cols="3">
-      <div class="link">주문 관리</div>
-    </v-col>
-  </v-row>
-  <v-col cols="6">
-    <div class="text-h6 text-center">정글 주문 관리</div>
-  </v-col>
-  <v-col cols="3">
+  <v-container>
+    <v-row>
+      <v-col cols="3">
+        <router-link to="/product">상품 관리</router-link>
+      </v-col>
+      <v-col cols="6">
+        <div class="text-h6 text-center">정글 주문 관리</div>
+      </v-col>
+      <v-col cols="3">
 
-  </v-col>
-  <v-row>
-    <OrderSector/>
-  </v-row>
-</v-container>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <OrderSector/>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
--- frontend/src/component/product/ProductPage.vue
+++ frontend/src/component/product/ProductPage.vue
@@ -1,6 +1,6 @@
 <script>
 import AddProductDialog from "@/component/product/AddProductDialog.vue";
-import ProductSector from "@/App.vue";
+import ProductSector from "@/component/product/ProductSector.vue";
 
 export default {
   name: "ProductPage",
@@ -9,22 +9,24 @@ export default {
 </script>
 
 <template>
-<v-container>
-  <v-row>
-    <v-col cols="3">
-      <div class="link">상품 관리</div>
-    </v-col>
-    <v-col cols="6">
-      <div class="text-h6 text-center">정글 재고 관리</div>
-    </v-col>
-    <v-col cols="3">
-      <AddProductDialog v-if="page === 'PRODUCT'"/>
-    </v-col>
-  </v-row>
-  <v-row>
-    <ProductSector/>
-  </v-row>
-</v-container>
+  <v-container>
+    <v-row>
+      <v-col cols="3">
+        <router-link to="/">주문 관리</router-link>
+      </v-col>
+      <v-col cols="6">
+        <div class="text-h6 text-center">정글 재고 관리</div>
+      </v-col>
+      <v-col cols="3">
+        <AddProductDialog v-if="page === 'PRODUCT'"/>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <ProductSector/>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
