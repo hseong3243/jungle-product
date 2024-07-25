94d899a1bb3e1d6d127f6e11d9a3e728959799a5
hseong3243 <hseong3243@gmail.com>
2024-07-25 23:48:39 +0900

    chore: axios 토큰 인터셉터 구현

--- frontend/src/axios/index.js
+++ frontend/src/axios/index.js
@@ -1,7 +1,28 @@
 import axios from "axios";
+import router from "@/router/router.js";
+import {useAuthStore} from "@/store/AuthStore.js";
 
 const api = axios.create();
 
 api.defaults.baseURL = "http://localhost:8080";
+api.defaults.withCredentials = true;
+
+api.interceptors.request.use(config => {
+    const authStore = useAuthStore();
+    console.log(authStore.getBearerToken)
+    if(authStore.isLogin) {
+        config.headers.Authorization = `Bearer ${authStore.accessToken}`;
+    }
+    return config;
+})
+
+api.interceptors.response.use(response => {
+    return response;
+}, error => {
+    const status = error.response.status;
+    if (status === 403) {
+        router.replace('/login');
+    }
+});
 
 export default api;
